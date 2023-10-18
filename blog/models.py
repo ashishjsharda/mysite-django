a787e8081e041b942a652e9cd16c284eabaed3e2
Ashish Sharda <ashishjsharda@gmail.com>
2023-10-18 07:29:41 -0400

    add 1- many relationship

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'dr', 'Draft'
         PUBLISHED = 'pu', 'Published'
     title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     date = models.DateTimeField()
     published = models.DateTimeField(default=timezone.now)
